3ce990c59cef6557c5e919fa09af90e98bd57467
Otmane <otmane.sofiane@hotmail.fr>
2020-12-14 16:31:41 +0100

    first commit

---
+++ src/main/java/com/example/ProjectBibliotheque/ProjectBibliothequeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectBibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBibliothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBibliothequeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjectBibliotheque/ProjectBibliothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectBibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBibliothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
