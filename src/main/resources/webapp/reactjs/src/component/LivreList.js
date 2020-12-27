4f82400cfbd92edba39c3827cc973b191e46e104
Otmane <otmane.sofiane@hotmail.fr>
2020-12-27 16:28:19 +0100

    Ajout du LivreList.js

---
+++ src/main/resources/webapp/reactjs/src/component/LivreList.js
@@ -0,0 +1,71 @@
+import * as react from "react";
+import {Card, Table} from "react-bootstrap";
+import axios from "axios";
+
+class  LivreList extends react.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            books :[]
+        };
+    }
+    componentDidMount() {
+        axios.get("http://localhost:8080/livres",{
+            headers: {"Access-Control-Allow-Origin": "*"}
+
+        })
+            .then(response => response.data)
+            .then((data) =>{
+                this.setState({books:data});
+            })
+
+
+
+    }
+    render() {
+        return (
+            <Card className={"border border-dark bg-dark text-white"}>
+
+                <Card.Header>Liste des livres </Card.Header>
+                <Card.Body>
+                    <Table bordered hover striped variant={"dark"}>
+
+                        <thead>
+                        <tr>
+                            <th>Identifiant</th>
+                            <th>Titre</th>
+                            <th>Prix</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.books.length === 0 ?
+                            <tr>
+                                <td className={"text-center"} colSpan="6">Aucun livre de disponible</td>
+                            </tr> :
+                            this.state.books.map((book) =>(
+                                <tr key = {book.id}>
+                                    <td>{book.id}</td>
+                                    <td>{book.titre}</td>
+                                    <td>{book.price}</td>
+                                </tr>
+                            ))
+
+                        }
+
+
+                        </tbody>
+                    </Table>
+
+                </Card.Body>
+
+            </Card>
+
+        );
+    }
+
+
+
+}
+
+export default LivreList;
